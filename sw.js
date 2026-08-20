c4cd728feafa9879f81e4aef6f17b66f0201cd04
tungoooo <136715455+tungoooo@users.noreply.github.com>
2026-08-20 11:25:35 +0800

    Publish offline personal workbench PWA

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "personal-workbench-v6";
+const ROOT = new URL("./", self.registration.scope).href;
+const SHELL = [ROOT, new URL("manifest.webmanifest", ROOT).href, new URL("favicon.svg", ROOT).href, new URL("og.png", ROOT).href];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(ROOT, copy));
+      return response;
+    }).catch(() => caches.match(ROOT)));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+    return response;
+  })));
+});
